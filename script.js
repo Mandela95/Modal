cd36be0f4b5ecdcea4cf5c7e846cd75cab976f2e
Mohamed El-Seady <96991597+Mandela95@users.noreply.github.com>
2023-03-10 03:34:01 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
-let modalContainer = document.getElementById("modal-container");
-let showModalBtn = document.getElementById("showModalBtn");
-let closeBtn = document.querySelector(".close-btn");
+let $modalContainer = document.getElementById("modal-container");
+let $showModalBtn = document.getElementById("showModalBtn");
+let $closeBtn = document.querySelector(".close-btn");
 
 // show modal
 function showModal() {
-  modalContainer.style.display = "block";
+  $modalContainer.style.display = "block";
 }
 
-showModalBtn.addEventListener("click", showModal);
+$showModalBtn.addEventListener("click", showModal);
 
 // hide modal
 function hideModal() {
-  modalContainer.style.display = "none";
+  $modalContainer.style.display = "none";
 }
 
-closeBtn.addEventListener("click", hideModal);
+$closeBtn.addEventListener("click", hideModal);
 
 // close when click outside modal
 function closeOutsideModal(e) {
-  if (e.target == modalContainer) {
-    modalContainer.style.display = "none";
+  if (e.target == $modalContainer) {
+    $modalContainer.style.display = "none";
     // alert("Are You Sure You Want To Close ?");
   }
 }
 window.addEventListener("click", closeOutsideModal);
 
-closeBtn.addEventListener("click", function () {
-  // alert("Are You Sure You Want To Close ?");
+// close modal when pressing escape
+window.addEventListener("keyup", (e) => {
+  if (e.keyCode === 27) {
+    hideModal();
+  }
 });
