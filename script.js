7b7b842f13ed932eb56ed5122ca6f262da69c3b6
Mohamed El-Seady <96991597+Mandela95@users.noreply.github.com>
2023-03-11 15:41:38 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 let $modalContainer = document.getElementById("modal-container");
+let $modalContent = document.querySelector(".modal-content");
 let $showModalBtn = document.getElementById("showModalBtn");
 let $closeBtn = document.querySelector(".close-btn");
+let $closeModalBtn = document.querySelector(".close-modal-btn");
+let $yes = document.querySelector(".yes");
+let $no = document.querySelector(".no");
 
 // show modal
 function showModal() {
   $modalContainer.style.display = "block";
+  $modalContent.style.opacity = "1";
 }
 
 $showModalBtn.addEventListener("click", showModal);
@@ -14,16 +19,31 @@ function hideModal() {
   $modalContainer.style.display = "none";
 }
 
-$closeBtn.addEventListener("click", hideModal);
-
 // close when click outside modal
 function closeOutsideModal(e) {
   if (e.target == $modalContainer) {
     $modalContainer.style.display = "none";
-    // alert("Are You Sure You Want To Close ?");
+    $closeModalBtn.style.display = "none";
   }
 }
+
 window.addEventListener("click", closeOutsideModal);
+
+// show modal inside modal
+$closeBtn.addEventListener("click", function () {
+  $closeModalBtn.style.display = "block";
+  $modalContent.style.opacity = ".5";
+});
+
+$yes.onclick = function () {
+  $modalContainer.style.display = "none";
+  $closeModalBtn.style.display = "none";
+};
+
+$no.onclick = function () {
+  $closeModalBtn.style.display = "none";
+  $modalContent.style.opacity = "1";
+};
 
 // show modal when pressing enter
 window.addEventListener("keyup", (e) => {
